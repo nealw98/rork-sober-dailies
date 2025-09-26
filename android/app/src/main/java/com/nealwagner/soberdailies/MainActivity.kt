a502d24a7b7a68b9ec6373fd1e963d45d25d8d61
nealw98 <nealw98@gmail.com>
2025-09-26 15:11:01 -0600

    Override MainActivity to handle missing React Native feature flags library

--- android/app/src/main/java/com/nealwagner/soberdailies/MainActivity.kt
+++ android/app/src/main/java/com/nealwagner/soberdailies/MainActivity.kt
@@ -3,6 +3,9 @@ package com.nealwagner.soberdailies
 import android.os.Build
 import android.os.Bundle
 import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
     override fun getMainComponentName(): String = "main"
@@ -13,5 +16,20 @@ class MainActivity : ReactActivity() {
             setTurnScreenOn(true)
         }
         super.onCreate(null)
+    }
+
+    override fun createReactActivityDelegate(): ReactActivityDelegate {
+        return object : DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled) {
+            override fun onCreate(savedInstanceState: Bundle?) {
+                // Skip feature flags initialization by not calling super
+                // This prevents the crash from missing libreact_featureflagsjni.so
+                try {
+                    super.onCreate(savedInstanceState)
+                } catch (e: UnsatisfiedLinkError) {
+                    // Ignore missing native library error
+                    // Continue with default React Native initialization
+                }
+            }
+        }
     }
 }
