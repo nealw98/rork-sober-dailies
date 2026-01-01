5112bc5d48e4eb7c03c4ee2ac0e34dd1e60cfcc4
nealw98 <nealw98@gmail.com>
2026-01-01 16:30:25 -0700

    style: Make Daily Reflections header gradient even darker

--- components/DailyReflection.tsx
+++ components/DailyReflection.tsx
@@ -384,7 +384,7 @@ export default function DailyReflection({ fontSize = 18, lineHeight, jumpToDate 
     <View style={styles.container}>
       {/* Gradient header block - darker version for better contrast */}
       <LinearGradient
-        colors={['#7BA3CC', '#8CBDC4', '#A8D4C4']}
+        colors={['#5A82AB', '#6B9CA3', '#7FB3A3']}
         style={styles.headerBlock}
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 1 }}
